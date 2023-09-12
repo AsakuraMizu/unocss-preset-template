803bb7096247d488117c082560a2bca65226416c
Asakura Mizu <asakuramizu111@gmail.com>
2023-09-13 02:35:13 +0800

    fix template name

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export interface FooOptions {}
 
 export default function presetFoo(_options: FooOptions = {}): Preset {
   return definePreset({
-    name: 'unocss-preset-starter',
+    name: 'unocss-preset-foo',
     rules: [['custom-rule', { color: 'red' }]],
     // Customize your preset here
   });
